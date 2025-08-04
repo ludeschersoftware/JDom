400e7a9dd32f2093820b93cec899011814c632d2
Johannes <github.finch886@passmail.net>
2025-08-05 01:15:10 +0200

    added missing exports

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { appendElement, appendElements, createElement, createElements } from "./dist/DomFactory";
+import TRef from "./dist/TRef";
 
 export {
     appendElement,
     appendElements,
     createElement,
     createElements,
+    TRef,
 };
